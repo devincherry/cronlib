2cb13178b0a0752e1d05cc26fe0057ff10ef3f96
Devin Cherry <devincherry@gmail.com>
2013-08-11 17:56:36 -0700

    Changed licensing from GPLv2 to LGPL v2.1, and added copyright notices to files.

--- __init__.py
+++ __init__.py
@@ -1,3 +1,25 @@
+####################################################################################
+#    Copyright (C) 2013  Devin Cherry
+#
+#    This library is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU Lesser General Public
+#    License as published by the Free Software Foundation; either
+#    version 2.1 of the License, or (at your option) any later version.
+#
+#    This library is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public
+#    License along with this library; if not, write to the Free Software
+#    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+#
+#
+# Devin Cherry
+# <devincherry@gmail.com>
+####################################################################################
+
 """cronlib
 
 A package for cron job handling functionality in Python. Adds cron parsing
@@ -13,7 +35,7 @@ and auditing functions, enabling a developer to quickly view/manage cron jobs.
 """
 
 __version__ = '0.1a'
-__author__ = 'Devin Cherry <devincherry^at^gmail^dot^com>'
+__author__ = 'Devin Cherry <devincherry@gmail.com>'
 
 
 ####
